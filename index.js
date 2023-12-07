f2e9602751f5f12055dba1632318db2b187fead5
qigmingli001 <qingmingli@DESKTOP-VL3RCLT.localdomain>
2023-12-07 10:52:17 -0800

    init backend

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only JavaScript",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      important: true
+    }
+]
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        response.json(note)
+
+    } else {
+        response.status(404).end()
+    }
+
+})
+
+app.post('/api/notes', (request, response) => {
+    const note = request.body
+    console.log(note)
+    response.json(note)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
